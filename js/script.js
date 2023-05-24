c0bb6713c1cbfa47f7dff4b42402485f0e4be44f
GiadaTonioni <giada.tonioni@gmail.com>
2023-05-24 10:50:10 +0200

    correzioni, non funzionante

--- js/script.js
+++ js/script.js
@@ -1,45 +1,56 @@
 //array
-let img = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'] ;
+const img = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'] ;
 console.log(img);
 
 let slider = document.querySelector('.slider');
 
 let slider_cont = '';
 
+let active_element = 0
+
 for (let i = 0; i < img.length; i++) {
 
     let visible_img = img[i];
     slider_cont += `
         <div class="slide">
-            <img src="${visible_img}/>
-        </div>`
+            <img src="./${visible_img}">
+        </div>`;
 }
 
 slider.innerHTML = slider_cont;
 
-let slides = document.getElementsByClassName('slide')
-console.log(slides)
-
-let active_element = 0
-slides[active_element].classList.add('active');
+let slides = ''
+document.getElementsByClassName('slide')[active_element].classList.add('active')
 
 let prev_button = document.querySelector('.prev')
 let next_button = document.querySelector('.next')
 
-next_button.addEventListener('click', function() {
-
-    if(active_element < (img.length - 1)) {
-        slides[active_element].classList.remove('active');
-        active_element++;
-        slides[active_element].classList.add('active');
-    }
-})
-
 prev_button.addEventListener('click', function() {
 
-    if(active_element < 0) {
-        slides[active_element].classList.remove('active');
-        active_element--;
-        slides[active_element].classList.add('active');
+    if(active_element === 0) {
+
+        active_element = img.length - 1;
     }
+    else {
+        active_element--;
+    }
+
+    document.querySelector('.slide.active').classList.remove('active')
+    document.getElementsByClassName('slide')[active_element].classList.add('active')
+        
+})
+
+next_button.addEventListener('click', function() {
+
+    if(active_element === img.length -1) {
+
+        active_element = 0;
+    }
+    else {
+        active_element++;
+    }
+
+    document.querySelector('.slide.active').classList.remove('active')
+    document.getElementsByClassName('slide')[active_element].classList.add('active')
+        
 })
